155bf493b32f59e13a79205590eec7ca87641c09
Surapong Kawkangploo <surapongkawkangploo@Surapongs-MacBook-Pro.local>
2020-03-15 11:30:37 +0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,34 @@
+// import App from 'next/app'
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import fetch from "node-fetch";
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  fetch
+});
+
+function MyApp({ Component, pageProps }) {
+  console.log("app");
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp;
